450a1bc6fefca11bc1eb32dc6edd65c55e39abb5
marija-p <mpopovic@ethz.ch>
2018-07-16 10:20:46 +0200

    Add results for diff. seeds

--- mav_ipp_sim/src/MATLAB/evals/ipp_eval.m
+++ mav_ipp_sim/src/MATLAB/evals/ipp_eval.m
@@ -1,28 +1,36 @@
-logger = [];
+%logger = [];
 
 % Environment dimensions [m].
 dim_x_env = 200;
 dim_y_env = 290;
 
 
-for i = 6:10
+for i = 20:30
     
     % Set the seed in the CMA-ES random number generator.
     [matlab_params, planning_params, opt_params, map_params] = ...
         load_params(dim_x_env, dim_y_env);
-    opt_params.seed = i;
+    opt_rosparams.seed = i;
     
     % Non-adaptive planning.
     planning_params.do_adaptive_planning = 0;
-    metrics = ipp_node_fun(matlab_params, planning_params, ...
-        opt_params, map_params);
-    logger.(['seed', num2str(i)]).('nonadaptive') = metrics;
+    try
+        metrics = ipp_node_fun(matlab_params, planning_params, ...
+            opt_params, map_params);
+        logger.(['seed', num2str(i)]).('nonadaptive') = metrics;
+    catch
+        continue;
+    end
     
     % Adaptive planning.
     planning_params.do_adaptive_planning = 1;
-    metrics = ipp_node_fun(matlab_params, planning_params, ...
-        opt_params, map_params);
-    logger.(['seed', num2str(i)]).('adaptive') = metrics;
+    try
+        metrics = ipp_node_fun(matlab_params, planning_params, ...
+            opt_params, map_params);
+        logger.(['seed', num2str(i)]).('adaptive') = metrics;
+    catch
+        continue;
+    end
     
     disp(['Evaluated Seed ', num2str(i)]);
     
