4826809918f804d3209218d594816f877d4c0d79
marija-p <mpopovic@ethz.ch>
2018-07-16 15:57:23 +0200

    Add map evolution for RIT-18 dataset.

--- mav_ipp_sim/src/MATLAB/evals/plot_path.m
+++ mav_ipp_sim/src/MATLAB/evals/plot_path.m
@@ -3,6 +3,7 @@ function [] = plot_path(path, planning_params)
 
 t = [];
 p = [];
+t_meas = [];
 p_meas = [];
 
 % Many polynomials.
@@ -18,12 +19,14 @@ if isfield(planning_params, 'control_points')
             planning_params.max_vel, ...
             planning_params.max_acc);
         [t_poly, p_poly] = sample_trajectory(trajectory, 0.1);
-        [~, p_meas_poly] = sample_trajectory(trajectory, ...
+        [t_meas_poly, p_meas_poly] = sample_trajectory(trajectory, ...
             1/planning_params.measurement_frequency);
         if (i == 1)
             t = [t; t_poly'];
+            t_meas = [t_meas; t_meas_poly'];
         else
             t = [t; t(end) + t_poly'];
+            t_meas = [t_meas; t_meas(end) + t_meas_poly'];
         end
         p = [p; p_poly];
         p_meas = [p_meas; p_meas_poly];
@@ -40,18 +43,19 @@ else
     trajectory = plan_path_waypoints(path, planning_params.max_vel, ...
         planning_params.max_acc);
     [t, p] = sample_trajectory(trajectory, 0.1);
-    [~, p_meas] = sample_trajectory(trajectory, ...
+    [t_meas, p_meas] = sample_trajectory(trajectory, ...
         1/planning_params.measurement_frequency);
     
 end
 
 hold on
 % Visualize trajectory.
-cline(p(:,1), p(:,2), p(:,3), t);
+ind = find(t == 100);
+cline(p(1:ind,1), p(1:ind,2), p(1:ind,3), t(1:ind));
 % Visualize control points.
 %scatter3(path(:,1), path(:,2), path(:,3), 140, 'xk');
 % Visualize measurements.
-colors_meas = linspace(0, t(end),size(p_meas,1));
+colors_meas = linspace(0, t_meas(end), size(p_meas,1));
 
 % Silly bug with 3 points.
 % https://ch.mathworks.com/matlabcentral/newsreader/view_thread/136731
@@ -59,20 +63,28 @@ if isequal(size(colors_meas),[1 3])
     colors_meas = colors_meas';
 end
 
-scatter3(p_meas(:,1), p_meas(:,2), p_meas(:,3), 60, colors_meas, 'filled');
+ind = find(t_meas == 100);
+scatter3(p_meas(1:ind,1), p_meas(1:ind,2), p_meas(1:ind,3), 30, ...
+    colors_meas(1:ind), 'filled');
 
 xlabel('x (m)')
 ylabel('y (m)')
 zlabel('z (m)')
 axis([-110 110 -160 160 0 200])
-grid minor
 colormap jet
+grid on
 c = colorbar;
 ylabel(c, 'Time (s)')
 view(3)
 %legend('Path', 'Control pts.', 'Meas. pts.', 'Location', 'northeast')
-legend('Path', 'Meas. pts.', 'Location', 'northeast')
+%legend('Path', 'Meas. pts.', 'Location', 'northeast')
+daspect([1 1 1])
+set(gca, 'FontName', 'Times')
+set(gca, 'FontSize', 11)
+set(gcf, 'Color', 'w')
+set(gcf, 'Position', [1, 1, 309, 399])
+%disp(p_meas)
 
-disp(p_meas)
+disp(ind)
 
 end
