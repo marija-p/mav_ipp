a962878b31c314a2404baf5be2e9a6079f6c65be
Julius Rückin <julius.rueckin@gmail.com>
2021-03-09 19:30:25 +0100

    setup docker-compose and third party installations for easy deployment

---
+++ deployment/install-third-party-dependencies.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/catkin/catkin_simple.git
+git clone https://github.com/ethz-asl/rotors_simulator.git
+git clone https://github.com/ethz-asl/mav_comm.git
+git clone https://github.com/ethz-asl/glog_catkin.git
+git clone https://github.com/ethz-asl/eigen_checks.git
+git clone https://github.com/ethz-asl/mav_control_rw.git
+git clone https://github.com/ethz-asl/eigen_catkin.git
