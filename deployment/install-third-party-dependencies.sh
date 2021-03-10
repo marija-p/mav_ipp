a275742708b3aa2beea33ac551f73ef87a2f8ec5
Julius Rückin <julius.rueckin@gmail.com>
2021-03-10 11:40:55 +0100

    check for already existing third party packages

--- deployment/install-third-party-dependencies.sh
+++ deployment/install-third-party-dependencies.sh
@@ -1,7 +1,16 @@
-git clone https://github.com/catkin/catkin_simple.git
-git clone https://github.com/ethz-asl/rotors_simulator.git
-git clone https://github.com/ethz-asl/mav_comm.git
-git clone https://github.com/ethz-asl/glog_catkin.git
-git clone https://github.com/ethz-asl/eigen_checks.git
-git clone https://github.com/ethz-asl/mav_control_rw.git
-git clone https://github.com/ethz-asl/eigen_catkin.git
+ETH_ASL_PACKAGE_DIRS="rotors_simulator mav_comm glog_catkin eigen_checks mav_control_rw eigen_catkin"
+for package_dir in $ETH_ASL_PACKAGE_DIRS; do
+    if [ ! -d "${package_dir}" ]; then
+        echo "Clone ${package_dir}..."
+        git clone https://github.com/ethz-asl/${package_dir}.git
+    else
+        echo "${package_dir} already exists"
+    fi
+done
+
+if [ ! -d "catkin_simple/" ]; then
+    echo "Clone catkin_simple..."
+    git clone https://github.com/catkin/catkin_simple.git
+else
+    echo "catkin_simple already exists"
+fi
